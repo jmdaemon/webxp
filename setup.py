95ac11c8e7363c9bce2c0cab3fb629896db1e5a5
Joseph Diza <josephm.diza@gmail.com>
2022-04-13 14:16:14 -0700

    Bump python ver

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="webxp",
-    version="0.2.0",
+    version="0.3.0",
     license='AGPL-3.0',
     author="Joseph Diza",
     author_email="josephm.diza@gmail.com",
